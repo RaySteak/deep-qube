a7104f29b41283d366c126ab5b2cb0f2cd12e1ae
wildug <wind1050@gmail.com>
2024-01-05 17:36:40 +0100

    Revert "added working directory imports of cube"
    This reverts commit a3dcbc9d4330eb36b8c3af32d87c0a570363d414.

--- deepcube/mcts.py
+++ deepcube/mcts.py
@@ -1,7 +1,5 @@
-import sys, os
-
-pathname = os.path.dirname(sys.argv[0])
-sys.path.append(pathname+"/../")
+import sys
+sys.path.append('../')
 
 import matplotlib.pyplot as plt
 from tqdm import tqdm
